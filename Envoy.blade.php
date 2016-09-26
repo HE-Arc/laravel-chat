df8d405f83532693aab8d20404d24af244d7b41a
Yoan Blanc <yoan@dosimple.ch>
2016-09-26 13:08:50 +0200

    Adding predis/predis

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,10 @@
 @servers(['web' => 'admin'])
 
 @task('deploy')
-    cd /var/www/html
+    cd /var/www/laravel-chat
     git pull origin master
+    composer install
+    npm install
+    gulp --production
+    php artisan migrate --no-interaction --force
 @endtask
