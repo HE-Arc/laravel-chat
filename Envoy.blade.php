423f95dae48bf9469dc56ab8065753e296df5b90
Yoan Blanc <yoan@dosimple.ch>
2016-11-29 18:07:12 +0100

    Composer install with --no-dev
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,6 +3,6 @@
 @task('deploy')
     cd /var/www/laravel-chat
     git pull origin master
-    composer install
+    composer install --no-dev
     php artisan migrate --no-interaction --force
 @endtask
